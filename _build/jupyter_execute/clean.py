4070e9cbb28dbfd07ff8752001cc0ed09f0070d8
Neal Caren <neal.caren@gmail.com>
2022-04-15 19:51:37 -0400

    new articles

--- _build/jupyter_execute/clean.py
+++ _build/jupyter_execute/clean.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[36]:
+# In[57]:
 
 
 fn = '/Users/nealcaren/Documents/GitHub/fightordie/_build/html/index.html'
 
 
-# In[37]:
+# In[58]:
 
 
 with open(fn,'r') as infile:
@@ -17,37 +17,45 @@ html = html.replace("<h1>Dare you Fight<a", "<h1>Dare you Fight: W.E.B. Du Bois 
 html = html.replace("<h1>Dare you Fight</h1>", "<h1>Dare you Fight:<br>W.E.B. Du Bois in The Crisis</h1>")
 
 
-# In[38]:
+# In[60]:
 
 
 start = html.find('<div class="section" id="table-of-contents">')
-stop = start + 6 + html[39032:].find('</div>')
+stop = start + 6 + html[start:].find('</div>')
 
 toc = html[start:stop]
+
+toc
+
+
+# In[61]:
+
 
 html = html.replace(toc, '\n')
 
 
-# In[39]:
+# In[62]:
 
 
 with open(fn,'w') as outfile:
     outfile.write(html)
 
 
-# In[43]:
+# In[63]:
 
 
 toc_fn = '/Users/nealcaren/Documents/GitHub/fightordie/_build/html/toc.html'
 with open(toc_fn,'r') as infile:
     toc_html = infile.read()
 toc_html = toc_html.replace('<p>PLACEHOLDER</p>', toc)
+with open(toc_fn,'w') as outfile:
+    outfile.write(toc_html)
 
 
-# In[44]:
+# In[51]:
 
 
-toc_html
+toc
 
 
 # In[ ]:
