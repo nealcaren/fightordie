6337e8f59b1c84fe0cbb236f24adeb74d4b7128e
Neal Caren <neal.caren@gmail.com>
2022-04-15 17:55:36 -0400

    new articles

--- _build/html/_sources/toc.md
+++ _build/html/_sources/toc.md
@@ -1,8 +1,6 @@
 # Table of contents
 
 
-
 ![](Images/cartoon_desk.png)
 
-```{tableofcontents}
-```
+PLACEHOLDER
--- _build/jupyter_execute/clean.py
+++ _build/jupyter_execute/clean.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[17]:
+# In[36]:
 
 
 fn = '/Users/nealcaren/Documents/GitHub/fightordie/_build/html/index.html'
 
 
-# In[18]:
+# In[37]:
 
 
 with open(fn,'r') as infile:
@@ -17,7 +17,18 @@ html = html.replace("<h1>Dare you Fight<a", "<h1>Dare you Fight: W.E.B. Du Bois 
 html = html.replace("<h1>Dare you Fight</h1>", "<h1>Dare you Fight:<br>W.E.B. Du Bois in The Crisis</h1>")
 
 
-# In[19]:
+# In[38]:
+
+
+start = html.find('<div class="section" id="table-of-contents">')
+stop = start + 6 + html[39032:].find('</div>')
+
+toc = html[start:stop]
+
+html = html.replace(toc, '\n')
+
+
+# In[39]:
 
 
 with open(fn,'w') as outfile:
